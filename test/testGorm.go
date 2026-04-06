1bca099bed2e11522f8e8ef1201ec0f2d7264ec5
Anyway <1172013878@qq.com>
2026-04-06 11:46:38 +0800

    修改链接测试代码注释

--- test/testGorm.go
+++ test/testGorm.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	fmt.Println("test gorm")
+	fmt.Println("Just test gorm")
 	db, err := gorm.Open(mysql.Open("root:123456@tcp(127.0.0.1:3306)/ginchat?charset=utf8mb4&parseTime=True&loc=Local"), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
